31d42d769ac169a237328ee6523dbee7d427cbf1
Memitaru <amandatwah@gmail.com>
2019-02-16 23:01:51 -0800

    Styled nav and set click event for drop down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // JS goes here
+
+let nav = document.querySelector('nav');
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {nav.classList.toggle('nav-open')})
