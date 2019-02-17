1c5e06619a87a068ccd7977d5be2b77b9b81b3f3
Memitaru <amandatwah@gmail.com>
2019-02-17 15:40:35 -0800

    Completed JS for nav and fixed icon toggle for nav menu

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@
 
 let nav = document.querySelector('nav');
 let hamburger = document.querySelector('.hamburger');
+let icon = document.querySelector('.icon');
 
 hamburger.addEventListener('click', () => {nav.classList.toggle('nav-open')})
 
+icon.addEventListener('click', () => {
+    if (icon.src.endsWith('nav-hamburger.png')) {
+        icon.src = 'img/nav-hamburger-close.png';
+    } else if (icon.src.endsWith('nav-hamburger-close.png')) {
+        icon.src = 'img/nav-hamburger.png';
+    }
+})
