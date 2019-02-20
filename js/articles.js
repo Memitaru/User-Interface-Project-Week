5ac289dc9fab8dc8c9c976a6997bbdba6e9887dc
Memitaru <amandatwah@gmail.com>
2019-02-19 18:06:22 -0800

    Finished JS for tabs, completed MVP

--- js/articles.js
+++ js/articles.js
@@ -1,6 +1,31 @@
-class Tab {
-    constructor (tabElement) {
-        this.tabElement = tabElement;
-        
+class TabLink {
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.article[data-tab="${this.data}"]`);
+        this.itemElement = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => this.select());
+    }
+
+    select(){
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach(link => link.classList.remove('active-tab'));
+        this.element.classList.add('active-tab');
+        this.itemElement.select();
     }
 }
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select(){
+        const items = document.querySelectorAll('.article');
+        items.forEach(item => item.classList.remove('active-article'));
+        this.element.classList.add('active-article');
+    }
+}
+
+const tabs = document.querySelectorAll('.tab');
+tabs.forEach(link => new TabLink(link));
